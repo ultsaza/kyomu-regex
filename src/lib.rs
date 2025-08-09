1c738670238a0d6387024864e2c71bad0173211c
kyomu <132206022+ultsaza@users.noreply.github.com>
2025-08-09 16:03:18 +0900

    feat: implement whole_match and  some derivative methods for KyomuRegex

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::char;
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum KyomuRegex {
     Char(char), 
@@ -7,3 +9,35 @@ pub enum KyomuRegex {
     Or(Box<KyomuRegex>, Box<KyomuRegex>),
     Star(Box<KyomuRegex>),
 }
+
+impl KyomuRegex {
+    pub fn whole_match(&self, input: &str) -> bool {
+        let mut reg = self.clone();
+        for ch in input.chars() {
+            reg = reg.derivative(ch);
+        }
+        return reg.match_eps();
+    }
+
+    pub fn derivative(&self, ch: char) -> KyomuRegex {
+        use KyomuRegex::*;
+        match self {
+            Char(c) => {
+                if *c == ch {
+                    Eps
+                } else {
+                    Empty
+                }
+            }
+
+            Eps => Empty,
+
+            Empty => Empty,
+
+            _ => {
+                unreachable!()
+            }
+        }
+    }
+}
+
