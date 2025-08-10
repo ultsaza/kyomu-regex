f4f4bb023b81b8a77ee576049fd0ca8bc15c0b5c
kyomu <132206022+ultsaza@users.noreply.github.com>
2025-08-11 02:17:22 +0900

    feat: enhance derivative handling with new s_or and s_concat functions

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,28 @@ impl KyomuRegex {
 
     pub fn derivative(&self, ch: char) -> KyomuRegex {
         use KyomuRegex::*;
+        fn s_or(left: KyomuRegex, right: KyomuRegex) -> KyomuRegex {
+            if left == right {
+                left
+            } else if left == Empty {
+                right 
+            } else if right == Empty {
+                left
+            } else {
+                Or(Box::new(left), Box::new(right))
+            }
+        }
+        fn s_concat(left: KyomuRegex, right: KyomuRegex) -> KyomuRegex {
+            if left == Eps {
+                right
+            } else if right == Eps{
+                left
+            } else if  left == Empty || right == Empty {
+                Empty
+            } else {
+                Concat(Box::new(left), Box::new(right))
+            }
+        }
         match self {
             Char(c) => {
                 if *c == ch {
@@ -83,3 +105,25 @@ impl KyomuRegex {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! chr { ($ch:expr) => { KyomuRegex::Char($ch) }; }
+    macro_rules! eps { () => { KyomuRegex::Eps }; }
+    macro_rules! empty { () => { KyomuRegex::Empty }; }
+    macro_rules! concat { ($left:expr, $right:expr) => { KyomuRegex::Concat(Box::new($left), Box::new($right)) }; }
+    macro_rules! or { ($left:expr, $right:expr) => { KyomuRegex::Or(Box::new($left), Box::new($right)) }; }
+    macro_rules! star { ($left:expr) => { KyomuRegex::Star(Box::new($left)) }; }
+
+    #[test]
+    fn simple_match() {
+        let r = concat!( star!( or!( chr!('a'), chr!('b') ) ),
+                      concat!( chr!('a'), chr!('b') ) );
+        assert!( r.whole_match("ab") );
+        assert!( r.whole_match("aab") );
+        assert!( r.whole_match("babab") );
+        assert!(!r.whole_match("aba") );
+    }
+
+}
